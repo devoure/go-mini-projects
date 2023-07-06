819bf07c4d35f8430f6ab028fd2c88c547ff4bba
Athumani Bakari <disguisedsandwich@gmail.com>
2023-07-06 17:15:27 +0300

    Finised CLI APP

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,44 +3,55 @@ package main
 import (
 	"booking-app/helper"
 	"fmt"
-	"strconv"
+	"sync"
+	"time"
 )
 
 var matchDay string = "Manchester United VS Chelsea"
-var bookedTickets = make([]map[string]string, 0)
+var bookedTickets = make([]UserDetails, 0)
 var remainingTickets uint = 50
+var wg = sync.WaitGroup{}
+
+type UserDetails struct {
+	firstName   string
+	secondName  string
+	email       string
+	userTickets uint
+}
 
 func main() {
-	for {
-		greetUser()
+	greetUser()
 
-		firstName, secondName, email, userTickets := getUserInput()
+	firstName, secondName, email, userTickets := getUserInput()
 
-		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, secondName, email, userTickets, remainingTickets)
+	isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, secondName, email, userTickets, remainingTickets)
 
-		if isValidTicketNumber && isValidEmail && isValidName {
+	if isValidTicketNumber && isValidEmail && isValidName {
+		bookTicket(firstName, secondName, email, userTickets)
 
-			bookTicket(firstName, secondName, email, userTickets)
+		wg.Add(1)
+		go sendTicket(userTickets, firstName, secondName, email)
 
-			firstNames := getFirstNames(bookedTickets)
-			fmt.Println(">>> Bookings Updated: ", firstNames)
-			fmt.Printf(">>> Bookings lists map: %v \n", bookedTickets)
-		} else {
-			fmt.Println(">>> Invalid Input")
-			if !isValidName {
-				fmt.Println(">>> Wrong Name Input")
-			}
-			if !isValidEmail {
-				fmt.Println(">>> Wrong Email Input")
-			}
-			if !isValidTicketNumber {
-				fmt.Println(">>> Wrong ticket number input")
-			}
+		firstNames := getFirstNames(bookedTickets)
+		fmt.Println(">>> Bookings Updated: ", firstNames)
+		fmt.Printf(">>> Bookings lists map: %v \n", bookedTickets)
+	} else {
+		fmt.Println(">>> Invalid Input")
+		if !isValidName {
+			fmt.Println(">>> Wrong Name Input")
 		}
-		if remainingTickets == 0 {
-			break
+		if !isValidEmail {
+			fmt.Println(">>> Wrong Email Input")
+		}
+		if !isValidTicketNumber {
+			fmt.Println(">>> Wrong ticket number input")
 		}
 	}
+	if remainingTickets == 0 {
+		fmt.Println("The match is booked out, sorry")
+	}
+
+	wg.Wait()
 }
 func greetUser() {
 	fmt.Println("Welcome to OLD TRAFFORD ticket system")
@@ -65,20 +76,29 @@ func getUserInput() (string, string, string, uint) {
 
 	return firstName, secondName, email, userTickets
 }
-func getFirstNames(bookedTickets []map[string]string) []string {
+func getFirstNames(bookedTickets []UserDetails) []string {
 	firstNames := []string{}
 	for _, booking := range bookedTickets {
-		firstNames = append(firstNames, booking["firstName"])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
 func bookTicket(firstName string, secondName string, email string, userTickets uint) {
-	var userDetails = make(map[string]string)
-	userDetails["firstName"] = firstName
-	userDetails["secondName"] = secondName
-	userDetails["email"] = email
-	userDetails["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+	var userDetails = UserDetails{
+		firstName:   firstName,
+		secondName:  secondName,
+		email:       email,
+		userTickets: userTickets,
+	}
 	bookedTickets = append(bookedTickets, userDetails)
 	remainingTickets = remainingTickets - userTickets
 	fmt.Println(">>> Ticket Booked")
+}
+func sendTicket(userTickets uint, firstName string, secondName string, email string) {
+	time.Sleep(5 * time.Second)
+	var ticketDetails = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, secondName)
+	fmt.Println(">>>>>>>>>>>>")
+	fmt.Printf("Sending ticket to email: %v \n %v", email, ticketDetails)
+	fmt.Println(">>>>>>>>>>>>")
+	wg.Done()
 }
