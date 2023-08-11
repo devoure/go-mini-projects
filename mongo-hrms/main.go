6db3fac62822d235e54c1eea24375c9971f1aec3
Athumani Bakari <disguisedsandwich@gmail.com>
2023-08-11 21:41:49 +0300

    Updated mongo functions

--- mongo-hrms/main.go
+++ mongo-hrms/main.go
@@ -80,16 +80,55 @@ func addNewEmployee(c *fiber.Ctx) error {
 
 	if err != nil {
 		return c.Status(500).SendString(err.Error())
-	}
+		}
+		filter := bson.D{{Key: "_id", Value: insertionResult.InsertedID}}
+		createdRecord := collection.FindOne(c.Context(), filter)
+		createdEmployee := &Employee{}
+		createdRecord.Decode(createdEmployee)
 
-	filter := bson.D{{Key: "_id", Value: insertionResult.InsertedID}}
-	createdRecord := collection.FindOne(c.Context(), filter)
+		return c.Status(201).JSON(createdEmployee)
+}
 
-	createdEmployee := &Employee{}
-	createdRecord.Decode(createdEmployee)
+func updateRecord(c *fiber.Ctx)(error){
+		idParam := c.Params("id")
 
-	return c.Status(201).JSON(createdEmployee)
+		employeeID, err := primitive.ObjectIDFromHex(idParam)
 
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		employee := new(Employee)
+
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+
+		query := bson.D{{Key: "_id", Value: employeeID}}
+		update := bson.D{
+			{Key: "$set",
+				Value: bson.D{
+					{Key: "name", Value: employee.Name},
+					{Key: "age", Value: employee.Age},
+					{Key: "salary", Value: employee.Salary},
+				},
+			},
+		}
+
+		err = mg.Db.Collection("employees").FindOneAndUpdate(c.Context(), query, update).Err()
+
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				return c.SendStatus(400)
+			}
+			return c.SendStatus(500)
+		}
+
+		employee.ID = idParam
+
+		return c.Status(200).JSON(employee)
+
+	})
 }
 
 func main() {
